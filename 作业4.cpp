082a0feacee5f3748cab5738369f3fc053e2db5d
Leizeyu666 <91419200+Leizeyu666@users.noreply.github.com>
2021-12-28 20:55:44 +0800

    Add files via upload

---
+++ 作业4.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int is_prime(long long n)
+{
+	if(n<=1)
+	  return 0;
+	for(int i=2;i*i<=n;i++)
+	  if(n%i==0)
+	    return 0;
+	return 1;  
+}
+long long power(long long x,long long n)
+{
+  return n == 0 ? 1 : x * power(x,n-1); 	
+}
+int main()
+{
+	for(long long p=2;p<=10000;p++)
+	  for(long long q=2;q<=10000;q++)
+	    if(is_prime(p)&&is_prime(q)&&is_prime(power(p,q)+power(q,p)))
+	      printf("p=%lld q=%lld\n",p,q);
+}
