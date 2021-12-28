505f813d75ffee5b60b836bc5122d9847ebb8059
Leizeyu666 <91419200+Leizeyu666@users.noreply.github.com>
2021-12-28 20:49:11 +0800

    Add files via upload

---
+++ 作业3.cpp
@@ -0,0 +1,33 @@
+//f(n)函数的定义为0~n中包含的1的个数
+//f(13)是0~13中包含1的有 1，10，11，12，13，所以f(13)=6
+//f(1) = 1。
+//求另一个f(n)=n的n值 
+//求另一个f(n)=n的n值
+// 2021-11-04 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int f(int n)
+{
+	int x=0;
+	char a[100];
+	for(int i=0;i<=n;i++)
+	{
+		sprintf(a,"%d",i);
+		for(int j=0;j<strlen(a);j++)
+		  if(a[j]=='1')
+		    x++;
+	}
+	return x;
+}
+int main()
+{
+	for(int i=2;i<100000000;i++)
+	{
+	  if(i==f(i))
+	  {
+	  	printf("%d",i);
+	  	break;
+	  }
+	}	  
+}
