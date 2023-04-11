f45186f0bbf95ab79cc4f69c5d2c652cde26e512
Ethy9160 <93207157+Ethylene9160@users.noreply.github.com>
2023-04-11 10:48:17 +0800

    Update 作业1.cpp

--- 作业1.cpp
+++ 作业1.cpp
@@ -5,7 +5,7 @@ int is_prime(int n)
 	
 	if(n<=1)
 	  return 0;
-	for(divisor=2;divisor*divisor<=n;divisor++)
+	for(divisor=2;divisor*divisor<=n;++divisor)
 	  if(n%divisor==0)
 	    return 0;
 	return 1;    
